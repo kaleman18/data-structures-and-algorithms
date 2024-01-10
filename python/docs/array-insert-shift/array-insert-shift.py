cccd30b1c6a025b6e200bbc78648faa895db078e
kaleman18 <kylealeman18@gmail.com>
2024-01-10 14:25:24 -0800

    finished code challenge 03

--- python/docs/array-insert-shift/array-insert-shift.py
+++ python/docs/array-insert-shift/array-insert-shift.py
@@ -1,28 +1,52 @@
 
 
 
-def insert_Shift_Array(given_list, num):
-    left_index = 0
-    right_index = len(given_list)-1
-    if len(given_list) % 2 == 0:
-        while left_index <= right_index:
-            if left_index == right_index:
-                given_list.insert(left_index, num)
-                break
+# def insert_Shift_Array(given_list, num):
+#     left_index = 0
+#     right_index = len(given_list)-1
+#     if len(given_list) % 2 == 0:
+#         while left_index <= right_index:
+#             if left_index == right_index:
+#                 given_list.insert(left_index, num)
+#                 break
             
-            left_index += 1
-            right_index -= 1
+#             left_index += 1
+#             right_index -= 1
+#     else:
+#         this_loop = True
+#         while this_loop:
+#             if left_index > right_index:
+#                 given_list.insert(left_index, num)
+#                 break
+#             left_index += 1
+#             right_index -= 1
+#     return given_list
+
+
+case1 = [2,4,6,-8] #[2,4,5,6,-8]
+num1 = 5
+
+case2 = [42,8,15,23,42] #[42,8,15,16,23,42]
+num2 = 16
+
+def insert_shift_array(given_list, num):
+  mid = 0
+  arrayLen = len(given_list)
+  if arrayLen % 2 == 0:
+    mid = arrayLen / 2
+  else: 
+    mid = arrayLen / 2 + 1
+  
+  leftArr = []
+  rightArr = []
+  
+  for i in range(arrayLen):
+    if i < mid:
+      leftArr += [given_list[i]]
     else:
-        this_loop = True
-        while this_loop:
-            if left_index > right_index:
-                given_list.insert(left_index, num)
-                break
-            left_index += 1
-            right_index -= 1
-    return given_list
+      rightArr += [given_list[i]]
+  
+  return leftArr + [num] + rightArr
 
-
-the_list = [42,8,15,23,42]
-insert_number = 16
-print(insert_Shift_Array(the_list, insert_number))
+print(insert_shift_array(case1, num1))
+print(insert_shift_array(case2, num2))
