f195f7f8451338ac4db7e234856f30d8ea7a6d56
kaleman18 <kylealeman18@gmail.com>
2024-01-18 18:06:37 -0800

    finished code challenge 07

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,6 +1,6 @@
 class Node:
-    def __init__(self, value=None):
-        self.value = value
+    def __init__(self, data=None):
+        self.data = data
         self.next = None
        
 
@@ -10,8 +10,8 @@ class LinkedList:
         self.head = None
         pass
 
-    def insert(self, value):
-        new_node = Node(value)
+    def insert(self, data):
+        new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
 
@@ -21,24 +21,107 @@ class LinkedList:
         cur = self.head
         string_rep = ""
         while cur:
-            string_rep += f"{{ {cur.value} }} -> "
+            string_rep += f"{{ {cur.data} }} -> "
             cur = cur.next
         
         string_rep += "NULL"
         return string_rep
     
-    def includes(self, value):
+    def includes(self, data):
         
         cur = self.head
         
         while cur != None:
-            if value == cur.value:
+            if data == cur.data:
                 return True
             cur = cur.next
             
         return False
+    
+    def append(self, data):
+        new_node = Node(data)
 
+        if self.head is None:
+            self.head = new_node
+            return
 
+        last_node = self.head
 
-class TargetError:
-    pass
+        while last_node.next:
+            last_node = last_node.next
+
+        last_node.next = new_node
+    
+    def insert_after(self, where, new_data):
+        new_node = Node(new_data)
+        if self.head is None:
+            self.head = new_node
+            raise TargetError("value not found")
+        
+
+        current = self.head
+        while current:
+            if current.data == where:
+                new_node.next = current.next
+                current.next = new_node
+                return
+            current = current.next
+        raise TargetError("value not found")
+
+    def insert_before(self, where, new_data):
+        new_node = Node(new_data)
+
+        if self.head is None:
+            self.head = new_node
+            raise TargetError("value not found")
+        
+
+        if self.head.data == where:
+            new_node.next = self.head
+            self.head = new_node
+            return
+        
+        current = self.head
+
+        while current.next:
+            if current.next.data == where:
+                new_node.next = current.next
+                current.next = new_node
+                return
+            current = current.next
+        raise TargetError("value not found")
+    
+    def len_iterative(self):
+
+        count = 0
+        cur_node = self.head
+
+        while cur_node:
+            count += 1
+            cur_node = cur_node.next
+        return count
+    
+    def kth_from_end(self, n):
+
+        traverse_counter = 0
+
+        total_len = self.len_iterative()
+
+        if n > total_len - 1 or n < 0:
+            raise TargetError("value not found")
+        
+        traverse_num_node = total_len - n
+
+        cur = self.head
+
+        while cur and traverse_num_node - 1 > 0:
+            cur = cur.next
+            traverse_num_node -= 1
+        return cur.data
+    
+    def zip_list()
+           
+        
+
+class TargetError(Exception):
+    print(Exception)
---
+++ python/docs/linked_list_insertions/README.md
@@ -0,0 +1,12 @@
+# Challenge Title
+linked list insertions
+
+## Whiteboard Process
+![WhiteBoard06](./401-code-challenge-06.JPG)
+
+## Approach & Efficiency
+These are linear methods. It is dependent on the size of the linked list and where specifically you want to add the new data.
+
+## Solution
+
+To run the code you need to call on the method in the class and pass in the value you want to insert and return a 
