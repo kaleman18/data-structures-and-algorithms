340c7ba86146e44e76e7067fbb0cb997b4b51ab8
kaleman18 <kylealeman18@gmail.com>
2024-01-29 08:47:33 -0800

    almost done

--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -4,13 +4,60 @@ class BinaryTree:
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.root = None
+        
 
-    def some_method(self):
-        # method body here
-        pass
+    def pre_order(self):
+        
+        def walk(node):
+            """Root -> Left -> Right"""
+            if node is None:
+                return []
+
+            result = [node.value]
+            """Recursive calls"""
+            left_result = walk(node.left)
+            right_result = walk(node.right)
+
+            return result + left_result + right_result
+        
+        return walk(self.root)
+    
+    def in_order(self):
+        
+        def walk(node):
+            """Left -> Root -> Right"""
+            if node is None:
+                return []
+
+            left_result = walk(node.left)
+            result = [node.value]
+            right_result = walk(node.right)
+
+            return left_result + result + right_result
+        
+        return walk(self.root)
+    
+    def post_order(self):
+        
+        def walk(node):
+            """Left -> Right -> Root"""
+            if node is None:
+                return []
+
+            left_result = walk(node.left)
+            right_result = walk(node.right)
+            result = [node.value]
+
+            return left_result + right_result + result
+        
+        return walk(self.root)
+
 
 
 class Node:
-    pass
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
